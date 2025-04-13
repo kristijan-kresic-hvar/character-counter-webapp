9aa2fb5b55a492b39ef13eaba31678e843771ba2
kristijan-kresic-hvar <kristijan.kresic.dev@gmail.com>
2025-04-13 06:48:41 +0200

    chore: cleanup

---
+++ src/components/app/component.tsx
@@ -0,0 +1,8 @@
+import styles from './style.module.css';
+import { FC } from 'react';
+
+const AppComponent: FC = () => {
+  return <div className={styles.container}>Hello character counter</div>;
+};
+
+export default AppComponent;
